ed249af52f5aa388e5a9914155f373675279099b
vitalis200721-gif/type29.git <vitalis200721@gmail>
2026-02-09 18:37:12 +0200

    ChangeServer

--- DevReviewAI/server.js
+++ DevReviewAI/server.js
@@ -1,5 +1,13 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 const app = express();
 app.use(cors());
